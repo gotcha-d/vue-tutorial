566204acdebabdbfcaafd3e4152ccdf5f0d2f073
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-10 00:07:13 +0900

    3.1.3  SFCの基本

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,47 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue"
+
+// テンプレート変数はrefで定義する
+const name = ref('田中太郎')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- これがマスタッシュ構文 -->
+  <h1>こんにちは、{{ name }}さん</h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
