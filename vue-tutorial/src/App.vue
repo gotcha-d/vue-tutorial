db5674c21ad228499515adad52141983389ce574
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-10 00:22:28 +0900

    3.2.1 refの動き

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref } from 'vue'
 
-// テンプレート変数はrefで定義する
-const name = ref('田中太郎')
+// 現在日時
+let now = new Date()
+let nowString = now.toLocaleTimeString()
+let timeString = nowString
+const refTimeString = ref(nowString)
+
+function changeTime() {
+  let newNow = new Date()
+  let newNowString = newNow.toLocaleTimeString()
+  timeString = newNowString
+  refTimeString.value = newNowString
+}
+setInterval(changeTime, 1000)
 </script>
 
 <template>
-  <!-- これがマスタッシュ構文 -->
-  <h1>こんにちは、{{ name }}さん</h1>
+  <h2>現在時刻： {{ timeString }}</h2>
+  <h2>現在時刻(ref)： {{ refTimeString }}</h2>
 </template>
