3ff0edda5159a376e99934b2ccc78436d8f76f37
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-10 23:56:47 +0900

    3.3.2 オブジェクトをまとめてリアクティブにするreactive()

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { reactive, computed } from 'vue';
 
 const radiusInit = Math.round(Math.random() * 10)
-const radius = ref(radiusInit)
-const PI = 3.14
-// 外部から与えられた計算結果をリアクティブデータにする方法が算出プロパティ
-// 算出プロパティは読み取り専用であることに注意する
-const area = computed((): number =>{
-  return radius.value * radius.value * PI
+const data = reactive({
+  PI: 3.14,
+  radius: radiusInit
 })
-const changeRadius = () => {
-  const newRadius = Math.round(Math.random() * 10)
-  radius.value = newRadius
+const area = computed(() => {
+  return data.radius * data.radius * data.PI
+})
+const changeRadius = (): Number => {
+  data.radius = Math.round(Math.random() * 10)
 }
 setInterval(changeRadius, 1000)
 </script>
 
 <template>
-円の半径を{{ radius }}、円周率を{{ PI }}とするとき円の面積は{{ area }}
+  <h2>半径{{ data.radius }}の円の面積は, 円周率が{{ data.PI }}のとき{{ area }}</h2>
 </template>
