47c6ae02cec6bc571d44ec2e828f2dd7d1d42bc1
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-11 21:14:38 +0900

    4.1.5 複数の属性にまとめてバインドする

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -5,6 +5,12 @@ const url = ref('https://vuejs.org/')
 const isSendButtonDisabled = ref(true)
 const widthOrHight = ref("height")
 const widthOrHightValue = ref(100)
+const imgAttributes = ref({
+  src: "/images/logo.svg",
+  alt: "vueのロゴ",
+  width: 75,
+  height: 100
+})
 </script>
 
 <template>
@@ -25,5 +31,11 @@ const widthOrHightValue = ref(100)
   <p>
     <!-- v-bindの引数にテンプレート変数を与えると、指定した属性に対して値をバインドできる -->
     <img src="./assets/logo.svg" alt="" v-bind:[widthOrHight]="widthOrHightValue">
+  </p>
+  <p>
+    <!-- 複数の属性をまとめてバインドする方法 -->
+    <img v-bind="imgAttributes">
+    <img v-bind="imgAttributes" title="rogoです">
+    <img v-bind="imgAttributes" height="100" width="100" title="rogoです">
   </p>
 </template>
