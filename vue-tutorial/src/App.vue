d1fb315798a34193dcdd8596b950f464d606513f
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-13 12:14:38 +0900

    4.2.5 キーイベントの修飾子

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -5,14 +5,34 @@ const msg = ref('未送信')
 const onFormSubmit = (): void => {
   msg.value = "送信されました"
 }
+
+const msg2 = ref("まだです")
+const onEnterKey = (): void => {
+  msg2.value = "エンターキーが押下されました"
+}
+const onRightButtonClick = (): void => {
+  msg2.value = "右クリックされました"
+}
+const onShiftClick = (): void => {
+  msg2.value = "シフト押しながらクリックされました"
+}
 </script>
 
 <template>
-  <!-- preventをつけていないと、フォームのデフォルトのイベントが実行されてページがリロードされてしまう -->
-  <!-- <form action="#" v-on:submit="onFormSubmit"> -->
-  <form action="#" v-on:submit.prevent="onFormSubmit">
-    <input type="text" required>
-    <button type="submit">送信</button>
-  </form>
-  <p>{{ msg }}</p>
+  <section>
+    <!-- preventをつけていないと、フォームのデフォルトのイベントが実行されてページがリロードされてしまう -->
+    <!-- <form action="#" v-on:submit="onFormSubmit"> -->
+    <!-- <form action="#" v-on:submit.prevent="onFormSubmit">
+      <input type="text" required>
+      <button type="submit">送信</button>
+    </form>
+    <p>{{ msg }}</p> -->
+  </section>
+
+  <p>{{ msg2 }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey">
+  <!-- keydown.enterだと、shift+enterもイベントハンドラが実行される。enterだけの場合にしたい場合はこれ -->
+  <input type="text" v-on:keydown.enter.exact="onEnterKey">
+  <button v-on:click.right="onRightButtonClick">右クリック</button>
+  <button v-on:click.shift="onShiftClick">シフト押しながらクリック</button>
 </template>
