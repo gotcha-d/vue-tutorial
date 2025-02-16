896cd2d00ba1a59a894d1cf39efbffcf55f881ff
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-16 23:01:52 +0900

    6.1.1 v-if基礎

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { computed, ref } from 'vue';
 
-const html = ref("<a href='https://vuejs.org'>Vue.jsのトップページ</a>")
+const number = ref(80)
+const showOrNot = computed((): boolean => {
+  let showOrNot = false
+  const rand = Math.round(Math.random() * 100)
+  if (rand >= 50 ) {
+    showOrNot = true
+  }
+  return showOrNot
+})
 </script>
 <template>
-  <!-- テンプレート変数はバインドするときに自動でエスケープする -->
-  <section>{{ html }}</section>
-  <section v-html="html"></section>
+  <p v-if="number >= 50">
+    条件に合致したので表示1
+  </p>
+  <!-- 可読性の観点からv-ifの中に複雑な式を書くのはNG -->
+  <p v-if="Math.round(Math.random() * 100) >= 50">
+    条件に合致したので表示2
+  </p>
+  <!-- 条件に合致した場合のみレンダリングされる -->
+  <p v-if="showOrNot">
+    条件に合致したので表示3
+  </p>
 </template>
