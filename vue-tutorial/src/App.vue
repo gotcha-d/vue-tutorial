9483f29797ac365bea9c0bab7ef3d76643016596
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-22 13:59:21 +0900

    6.2.6 Mapとオブジェクトの組みわわせのループ

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -46,6 +46,13 @@ interface Cocktail {
   name: string;
   price: number;
 }
+
+const cocktailMapObjInit = new Map<number, Cocktail>()
+cocktailMapObjInit.set(2345, {id: 2345, name: "ホワイトレディ", price: 1200})
+cocktailMapObjInit.set(4412, {id: 4412, name: "ブルーハワイ", price: 1500})
+cocktailMapObjInit.set(6792, {id: 6792, name: "ニューヨーク", price: 1100})
+cocktailMapObjInit.set(8429, {id: 8429, name: "マティーニ", price: 1500})
+const cocktailMapObj = ref(cocktailMapObjInit)
 </script>
 
 <template>
@@ -117,6 +124,14 @@ interface Cocktail {
           <dd>{{ value }}</dd>
         </template>
       </dl>
+    </li>
+  </ul>
+  <ul>
+    <li
+      v-for="[id, cocktailItem ] in cocktailMapObj"
+      v-bind:key="id"
+    >
+      {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}
     </li>
   </ul>
 </template>
