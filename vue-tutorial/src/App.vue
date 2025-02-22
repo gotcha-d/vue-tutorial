8069b8f735ad6ac61b2ab5a7e464d02c0891f172
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-22 22:52:59 +0900

    7.1.2 watchEffect

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
-import { ref, computed } from "vue"
-
-const cocktailListDataInit = new Map<number, Cocktail>()
-cocktailListDataInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200})
-cocktailListDataInit.set(2, {id: 1, name: "ブルーハワイ", price: 1500})
-cocktailListDataInit.set(3, {id: 3, name: "ニューヨーク", price: 1100})
-cocktailListDataInit.set(4, {id: 4, name: "マティーニ", price: 1500})
+import { ref, computed, watchEffect } from "vue"
 
 const cocktailListNo = ref(1)
-const priceMsg = computed((): string => {
-  // 算出関数は便利だが、これはあくまで算出に使用する変数がスクリプトブロック内の変数だった時だけ
-  // 算出関数で利用するデータがインターネットから取得した情報だった場合は非常に重い
-  const cocktail = cocktailListDataInit.get(cocktailListNo.value)
-  let msg = "該当のカクテルはありません"
-  if (cocktail !== undefined) {
-    msg = `該当のカクテルは${cocktail.name}で、価格は${cocktail.price}です`
-  }
-  return msg
+const priceMsg = ref("")
+watchEffect((): void => {
+  // リアクティブ変数に応じて実行される処理
+  priceMsg.value = getCocktailInfo(cocktailListNo.value)
 })
 
 interface Cocktail {
@@ -25,6 +14,20 @@ interface Cocktail {
   price: number;
 }
 
+function getCocktailInfo(cocktailNo: number) {
+  const cocktailListDataInit = new Map<number, Cocktail>()
+  cocktailListDataInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200})
+  cocktailListDataInit.set(2, {id: 1, name: "ブルーハワイ", price: 1500})
+  cocktailListDataInit.set(3, {id: 3, name: "ニューヨーク", price: 1100})
+  cocktailListDataInit.set(4, {id: 4, name: "マティーニ", price: 1500})
+
+  const cocktail = cocktailListDataInit.get(cocktailNo)
+  let msg = "該当のカクテルはありません"
+  if (cocktail !== undefined) {
+    msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円`
+  }
+  return msg
+}
 setInterval(() => {
   cocktailListNo.value = Math.round(Math.random() * 3) + 1
 }, 1000)
