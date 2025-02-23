842bafa0861f97fb9912c796f5d577fc0e00a48a
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-23 09:58:12 +0900

    7.4.1 OptionsAPIの基本構文

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,62 +1,33 @@
 <script lang="ts">
 import { defineComponent, ref, computed, reactive, toRefs } from "vue"
 
-// Vueバージョン3.2より前の書き方。defineComponentによってコンポーネントの処理内容を記載する
+// OpetionsAPI3.2より前の書き方
 export default defineComponent({
   name: "App", // コンポーネント名
-  setup() {
-    // compositionAPIでscriptブロックに書いていた内容
-    const cocktailDetailListInit = new Map<number, Cocktail>()
-    cocktailDetailListInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200})
-    cocktailDetailListInit.set(2, {id: 2, name: "ブルーハワイ", price: 1500})
-    cocktailDetailListInit.set(3, {id: 3, name: "ニューヨーク", price: 1100})
-    cocktailDetailListInit.set(4, {id: 4, name: "マティーニ", price: 1500})
-
-    const cocktailNo = ref(1)
-    const priceMsg = computed((): string => {
-      const cocktail = cocktailDetailListInit.get(cocktailNo.value)
-      let msg = "該当のカクテルはありません"
-      if (cocktail !== undefined) {
-        msg = `該当のカクテルは${cocktail.name}で、価格は${cocktail.price}円`
-      }
-      return msg
-    })
-
-    const data = reactive({
-      PI: 3.14,
-      radius: Math.round(Math.random() * 10)
-    })
-    const area = computed(() => {
-      return data.radius * data.radius * data.PI
-    })
-    setInterval(() => {
-      cocktailNo.value = Math.round(Math.random() * 3) + 1
-
-      data.radius = Math.round(Math.random() * 10)
-
-    }, 1000)
-    
-    // 戻り値としてリアクティブ変数をリターンして、templateブロックで利用できるようになる
+  data() {
+    // これまでテンプレート変数として定義した値を用意。OpetionsAPIではデータプロパティという
     return {
-      cocktailNo,
-      priceMsg,
-      ...toRefs(data),
-      area
+      width: Math.round(Math.random() * 10),
+      height: Math.round(Math.random() * 10)
     }
-
+  },
+  computed: {
+    // 算出プロパティを定義
+    area(): number {
+      return this.width * this.height
+    }
+  },
+  methods: {
+    // メソッド
+    change(): void {
+      this.width = Math.round(Math.random() * 10),
+      this.height = Math.round(Math.random() * 10)
+    }
   }
 })
-
-interface Cocktail {
-  id: number;
-  name: string;
-  price: number;
-}
 </script>
 
 <template>
-  <p>現在のカクテル番号： {{ cocktailNo }}</p>
-  <p>{{ priceMsg }}</p>
-  <hr>
-  <p>半径{{ radius }}の面積は{{ area }}</p>
+  <p>縦が{{ height }}、横が{{ width }}の長方形の面積は{{ area }}</p>
+  <button @click="change">値を変更</button>
 </template>
