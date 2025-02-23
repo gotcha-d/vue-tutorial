ef7915e408af74a8d6c4d9a02a5d41e1f63363eb
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-24 00:05:31 +0900

    7.4.5 optionsAPI 使いづらいところのハンズオン(まともに動かないコード)

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,33 +1,49 @@
 <script lang="ts">
-import { defineComponent, ref, computed, reactive, toRefs } from "vue"
+import { defineComponent } from 'vue';
 
-// OpetionsAPI3.2より前の書き方
 export default defineComponent({
-  name: "App", // コンポーネント名
   data() {
-    // これまでテンプレート変数として定義した値を用意。OpetionsAPIではデータプロパティという
     return {
-      width: Math.round(Math.random() * 10),
-      height: Math.round(Math.random() * 10)
+      cocktailNo: 1,
+      priceMsg: ""
     }
   },
-  computed: {
-    // 算出プロパティを定義
-    area(): number {
-      return this.width * this.height
+  watch: {
+    cocktailNo(newVal: number, oldVal: number) {
+      const cocktailDetailList = new Map<number, Cocktail>()
+      cocktailDetailList.set(1, {id: 1, name: "ホワイトレディ", price: 1200})
+      cocktailDetailList.set(2, {id: 2, name: "ブルーハワイ", price: 1200})
+      cocktailDetailList.set(3, {id: 3, name: "ニューヨーク", price: 1200})
+      cocktailDetailList.set(4, {id: 4, name: "マティーニ", price: 1200})
+
+      const cocktail = cocktailDetailList.get(this.cocktailNo)
+      let msg = "該当のカクテルはありません"
+      if (cocktail !== undefined) {
+        msg = `該当のカクテルは${cocktail.name}で価格は${cocktail.price}円`
+      }
+      this.priceMsg = msg
     }
   },
-  methods: {
-    // メソッド
-    change(): void {
-      this.width = Math.round(Math.random() * 10),
-      this.height = Math.round(Math.random() * 10)
+  created(): void {
+    () => {
+      setInterval(
+        (): void => {
+          console.log(this)
+          // this.cocktailNo = Math.round(Math.random() * 3) + 1
+        }, 1000
+      )
     }
   }
 })
+
+interface Cocktail {
+  id: number;
+  name: string;
+  price: number;
+}
 </script>
 
 <template>
-  <p>縦が{{ height }}、横が{{ width }}の長方形の面積は{{ area }}</p>
-  <button @click="change">値を変更</button>
+  <p>現在のカクテル番号: {{ cocktailNo }}</p>
+  <p>{{ priceMsg }}</p>
 </template>
