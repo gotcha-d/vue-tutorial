8f8e217d4fac871bf6640c7e824b025538de0d39
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-24 10:20:18 +0900

    8.1.1 子コンポーネントの呼び出し

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,49 +1,18 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  data() {
-    return {
-      cocktailNo: 1,
-      priceMsg: ""
-    }
-  },
-  watch: {
-    cocktailNo(newVal: number, oldVal: number) {
-      const cocktailDetailList = new Map<number, Cocktail>()
-      cocktailDetailList.set(1, {id: 1, name: "ホワイトレディ", price: 1200})
-      cocktailDetailList.set(2, {id: 2, name: "ブルーハワイ", price: 1200})
-      cocktailDetailList.set(3, {id: 3, name: "ニューヨーク", price: 1200})
-      cocktailDetailList.set(4, {id: 4, name: "マティーニ", price: 1200})
-
-      const cocktail = cocktailDetailList.get(this.cocktailNo)
-      let msg = "該当のカクテルはありません"
-      if (cocktail !== undefined) {
-        msg = `該当のカクテルは${cocktail.name}で価格は${cocktail.price}円`
-      }
-      this.priceMsg = msg
-    }
-  },
-  created(): void {
-    () => {
-      setInterval(
-        (): void => {
-          console.log(this)
-          // this.cocktailNo = Math.round(Math.random() * 3) + 1
-        }, 1000
-      )
-    }
-  }
-})
-
-interface Cocktail {
-  id: number;
-  name: string;
-  price: number;
-}
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue';
 </script>
 
 <template>
-  <p>現在のカクテル番号: {{ cocktailNo }}</p>
-  <p>{{ priceMsg }}</p>
+  <h1>コンポーネント基礎</h1>
+  <section>
+    <h2>コンポーネント1個</h2>
+    <OneSection />
+  </section>
 </template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
