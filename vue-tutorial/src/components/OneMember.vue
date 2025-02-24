e2706d90b1424b56c3b1c2cdd2a6b01b513f6df3
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-24 11:38:49 +0900

    8.4. ¥3 propsのデフォルト値

--- vue-tutorial/src/components/OneMember.vue
+++ vue-tutorial/src/components/OneMember.vue
@@ -8,19 +8,14 @@ interface Props {
   points: number;
   note?: string;
 }
-const props = defineProps<Props>()
+const props = withDefaults(
+  defineProps<Props>(),
+  {note: "--"}
+)
 
 // このコンポーネント内で利用するポイント数のテンプレート変数
 // 親コンポーネントから受け取ったデータを子で変更したいときは、独自の変数を用意して加工する。
 const localPoints = ref(props.points)
-// このコンポーネント内で加工する算出プロパティ
-const localNote = computed((): string => {
-  let localNote = props.note
-  if (localNote == undefined) {
-    localNote = "--"
-  }
-  return localNote
-})
 
 // 対象会員の保有ポイントが変化する。ただし、親コンポーネントの合計ポイントは変化しない
 const pointUp = () => {
@@ -39,7 +34,7 @@ const pointUp = () => {
       <dt>保有ポイント</dt>
       <dd>{{ localPoints }}</dd>
       <dt>備考</dt>
-      <dd>{{ localNote }}</dd>
+      <dd>{{ note }}</dd>
     </dl>
     <button @click="pointUp">ポイント加算</button>
   </section>
