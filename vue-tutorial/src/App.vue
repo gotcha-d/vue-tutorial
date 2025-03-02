2fea6a940c3298b01dd0150467590c2aee1f1f13
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-02 17:57:10 +0900

    9.3.1 スコープ付きスロット

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,34 +1,19 @@
-<script setup lan="ts">
-import { ref } from 'vue';
+<script setup lang="ts">
 import OneSection from './components/OneSection.vue';
-
-const taroProblemsInit = [
-  "電話が通じません。",
-  "留守です"
-]
-const taro = ref("田中太郎")
-const taroProblems = ref(taroProblemsInit)
-const jiro = ref("鈴木次郎")
 </script>
 
 <template>
-  <h2>SLotの利用</h2>
-  <OneSection :name="taro">
-    <template v-slot:default>
-      <p>問題発生したよ</p>
-    </template>
-    <template v-slot:etail>
-      <ul>
-        <li v-for="problem in taroProblems">
-          {{ problem }}
-        </li>
-      </ul>
-    </template>
-  </OneSection>
-  <OneSection :name="jiro">
-    <!-- すべてのSlotに対して子コンポーネントに渡すHTMLを指定する必要はない -->
-    <template #default>
-      <p>微妙に問題発生</p>
-    </template>
-  </OneSection>
+  <section>
+    <OneSection>
+      <template v-slot:default="slotProps">
+        <dl>
+          <dt>名前</dt>
+          <dd>{{ slotProps.memberInfo.name }}</dd>
+          <dt>状況</dt>
+          <dd>{{ slotProps.memberInfo.state }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+    <OneSection />
+  </section>
 </template>
