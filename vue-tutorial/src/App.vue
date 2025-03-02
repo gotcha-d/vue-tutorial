e03ccc50062c7a2d82856c1b9fc90464cc423033
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-02 17:29:26 +0900

    9.1.5 親でのレンダリング結果を子に渡す

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -2,14 +2,26 @@
 import { ref } from 'vue';
 import OneSection from './components/OneSection.vue';
 
+const taroProblemsInit = [
+  "電話が通じません。",
+  "留守です"
+]
 const taro = ref("田中太郎")
+const taroProblems = ref(taroProblemsInit)
 const jiro = ref("鈴木次郎")
 </script>
 
 <template>
   <h2>SLotの利用</h2>
   <OneSection :name="taro">
-    <p>{{ taro }}さんは連絡がつきません</p>
+    <ul>
+      <li
+        v-for="problems of taroProblems"
+        v-bind:key="problems"
+      >
+        {{ problems }}
+      </li>
+    </ul>
   </OneSection>
   <OneSection :name="jiro"/>
 </template>
