8dd577a4be21b6218b626893c9c23b0b8dd51028
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-02 18:31:01 +0900

    9.4 動的コンポーネント

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
-import OneSection from './components/OneSection.vue';
+import { ref } from "vue"
+import Input from "./components/Input.vue";
+import Radio from "./components/Radio.vue";
+import Select from "./components/Select.vue";
+
+// 現在表示させるコンポーネント
+const currentComp = ref(Input)
+// 現在表示させるコンポーネント名
+const currentCompName = ref("Input")
+
+const compList = [Input, Radio, Select]
+const compNameList = ["Input", "Radio", "Select"]
+let currentCompIndex = 0
+
+const switchComp = (): void => {
+  currentCompIndex++
+
+  // 規定値以上の場合はリセットさせる
+  if (currentCompIndex > 2) {
+    currentCompIndex = 0
+  }
+
+  // コンポーネント切り替え
+  currentComp.value = compList[currentCompIndex]
+  currentCompName.value = compNameList[currentCompIndex]
+}
 </script>
 
 <template>
-  <section>
-    <OneSection>
-      <template v-slot:default="slotProps">
-        <dl>
-          <dt>名前</dt>
-          <dd>{{ slotProps.memberInfo.name }}</dd>
-          <dt>状況</dt>
-          <dd>{{ slotProps.memberInfo.state }}</dd>
-        </dl>
-      </template>
-    </OneSection>
-    <OneSection />
-  </section>
+  <p>コンポーネント名: {{ currentCompName }}</p>
+  <!-- <KeepAlive> -->
+    <component v-bind:is="currentComp" />
+  <!-- </KeepAlive> -->
+  <button v-on:click="switchComp">切り替え</button>
 </template>
---
+++ vue-tutorial/src/components/Radio.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const memberType = ref(1)
+</script>
+
+<template>
+  <label for="">
+    <input type="radio" name="memberType" v-model="memberType" value="1">
+    通常会員
+  </label>
+  <label for="">
+    <input type="radio" name="memberType" v-model="memberType" value="2">
+    特別会員
+  </label>
+  <label for="">
+    <input type="radio" name="memberType" v-model="memberType" value="3">
+    優良会員
+  </label>
+  <br>
+  <p>選択されたラジオボタン： {{ memberType }}</p>
+</template>
