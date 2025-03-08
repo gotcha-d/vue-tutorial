0c3ba5c4e438091aedb8930a91c1ca6265a81054
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-08 17:03:10 +0900

    セッションストレージからのデータ取得

--- vue-tutorial/src/store/member.ts
+++ vue-tutorial/src/store/member.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import type { Member } from "@/interfaces";
+import MemberAdd from "@/views/member/MemberAdd.vue";
 
 interface State {
   memberList: Member[]
@@ -23,12 +24,14 @@ export const useMemberStore = defineStore(
       }
     },
     actions: {
-      initList(): void {
-        this.memberList.set(33456, {id: 33456, name: "田中太郎", email: "tanaka@example.com", points: 35, note: "初回入会特典あり"})
-        this.memberList.set(47783, {id: 47783, name: "鈴木次郎", email: "mue@exmaple.jp", points:53})
-      },
-      addMember(member: Member): void {
-        this.memberList.set(member.id , member)
+      prepareMemberList(): void {
+        let memberList: Map<number, Member> = new Map<number, Member>()
+        const memberListJsonStr = sessionStorage.getItem("memberItem")
+        if (memberListJsonStr !== undefined && memberListJsonStr !== null) {
+          const memberListJson = JSON.parse(memberListJsonStr)
+          memberList = new Map<number, Member>(memberListJson)
+        }
+        this.memberList = memberList
       }
     }
   }
