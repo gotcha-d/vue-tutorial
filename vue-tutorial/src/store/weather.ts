6c9b62d15016b2b27a6787b3a2515e0b10384b31
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-08 20:02:48 +0900

    12.4 webアクセスコード以外の記述

--- vue-tutorial/src/store/weather.ts
+++ vue-tutorial/src/store/weather.ts
@@ -7,6 +7,9 @@ export interface City {
 
 interface State {
   cityList: Map<string, City>
+  selectedCity: City
+  isLoading: boolean
+  weatherDescription: string
 }
 
 export const useWeatherStore = defineStore(
@@ -14,7 +17,10 @@ export const useWeatherStore = defineStore(
   {
     state : (): State => {
       return {
-        cityList: new Map<string, City>()
+        cityList: new Map<string, City>(),
+        selectedCity: {name: "", q: ""},
+        isLoading: true,
+        weatherDescription: ""
       }
     },
     getters: {
@@ -25,6 +31,10 @@ export const useWeatherStore = defineStore(
         this.cityList.set("Osaka", {name: "大阪", q: "Osaka"})
         this.cityList.set("Kobe", {name: "神戸", q: "Kobe"})
         this.cityList.set("Himeji", {name: "姫路", q: "Himeji"})
+      },
+      async reactiveWeatherInfo(id: string) {
+        this.selectedCity = this.cityList.get(id) as City
+        // webアクセスコードを書く
       }
     }
   }
--- vue-tutorial/src/views/WeatherInfo.vue
+++ vue-tutorial/src/views/WeatherInfo.vue
@@ -1,6 +1,42 @@
 <script setup lang="ts">
+import { useWeatherStore, type City } from '@/store/weather';
+import { RouterLink } from 'vue-router';
+import { computed } from 'vue';
+
+interface Props {
+  id: string
+}
+const props = defineProps<Props>()
+
+const weatherStore = useWeatherStore()
+weatherStore.reactiveWeatherInfo(props.id)
+
+const isLoading = computed(
+  (): boolean => {
+    return weatherStore.isLoading
+  }
+)
+const selectedCity = computed(
+  (): City => {
+    return weatherStore.selectedCity
+  }
+)
+const weatherDescription = computed(
+  (): string => {
+    return weatherStore.weatherDescription
+  }
+)
 </script>
 
 <template>
-  
+  <p v-if="isLoading">データ取得中...</p>
+  <section v-else>
+    <h2>{{ selectedCity.name }}の天気</h2>
+    <p>{{ weatherDescription }}</p>
+  </section>
+  <p>リストに
+    <RouterLink :to="{name: 'CityList'}">
+      戻る
+    </RouterLink>
+  </p>
 </template>
