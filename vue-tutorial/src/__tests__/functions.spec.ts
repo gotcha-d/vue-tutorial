f6f73a0b4396e2fe500f4b6d89f138beb7a30e4d
gotcha-d <gotcha.privatemail@gmail.com>
2025-03-08 21:35:28 +0900

    13.1.7 テストケースの追加

--- vue-tutorial/src/__tests__/functions.spec.ts
+++ vue-tutorial/src/__tests__/functions.spec.ts
@@ -10,5 +10,20 @@ describe("functions.ts.のdivideTwoNum()関数のテスト", () => {
         expect(actual).toBe(expected)
       }
     )
+    test("分子が0の場合のテスト", () => {
+      const num1 = 0
+      const num2 = 3
+      const actual = divideTwoNums(num1, num2)
+      const expected = 0
+      expect(actual).toBe(expected)
+    })
+    
+    test("分母が0の場合のテスト", () => {
+      const num1 = 3
+      const num2 = 0
+      const actual = divideTwoNums(num1, num2)
+      const expected = Infinity
+      expect(actual).toBe(expected)
+    })
   }
 )
